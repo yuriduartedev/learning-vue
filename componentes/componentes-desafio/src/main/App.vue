0d8ad360ac5d06838e42fe39dd1e4e19040fb253
Yuri Duarte <yuriduartedev@gmail.com>
2019-08-02 12:42:02 -0300

    Introdução a componentes

---
+++ componentes/componentes-desafio/src/main/App.vue
@@ -0,0 +1,47 @@
+<template>
+	<v-app>
+		<!-- Navegação -->
+		<app-navegacao />
+
+		<!-- Toolbar -->
+		<app-toolbar />
+
+		<!-- Carrossel -->
+		<app-carrossel />
+
+		<!-- Footer -->
+		<app-footer />
+	</v-app>
+</template>
+
+<script>
+// import Navegacao from './Navegacao'
+import Footer from '@/components/template/Footer'
+import Carrossel from '@/components/widgets/Carrossel'
+import Toolbar from '@/components/template/Toolbar'
+import Navegacao from '@/components/template/Navegacao'
+
+export default {
+	components: { 
+		'app-footer': Footer,
+		'app-carrossel': Carrossel,
+		'app-toolbar': Toolbar,
+		'app-navegacao': Navegacao
+	}, 
+
+	data() {
+		return {
+			itensMenu: [
+				{titulo: 'Início', icon: 'dashboard'},
+				{titulo: 'Sobre', icon: 'question_answer'}
+			],
+			fotos: [
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'},
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'},
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'},
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'}
+			]
+		}
+	}
+}
+</script>
