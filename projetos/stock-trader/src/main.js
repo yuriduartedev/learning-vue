35c7377e4496a2764dd1c2f9aaf078666484a586
Yuri Duarte <yuriduartedev@gmail.com>
2019-09-28 10:34:33 -0300

    Finaliza projeto stock-trader

---
+++ projetos/stock-trader/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store/store'
+
+import './plugins/vuetify'
+import axios from './plugins/axios'
+
+Vue.config.productionTip = false
+
+Vue.filter('currency', value => {
+	return 'R$ ' + value.toLocaleString()
+}) 
+
+new Vue({
+	router,
+	store,
+	render: h => h(App),
+}).$mount('#app')
---
+++ projetos/stock-trader/src/store/actions.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+
+export default {
+  loadData({ commit }) {
+    Vue.prototype.$http.get('data.json').then(res => {
+      const data = res.data
+      if(data) {
+        commit('setStocks', data.stocks)
+        commit('setPortfolio', {
+          funds: data.funds,
+          stockPortfolio: data.stockPortfolio
+        })
+      }
+    })
+  }
+}
